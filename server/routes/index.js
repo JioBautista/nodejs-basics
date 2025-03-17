eb96ecf8124fa335cd9864f9109bec6e2c0af766
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2025-03-17 16:18:58 -0400

    Updates

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,14 @@
+require("dotenv").config();
+
 const express = require("express");
+const postgres = require("postgres");
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Hello World!" });
+router.get("/", async function (req, res, next) {
+  const sql = postgres(`${process.env.DATABASE_URL}`);
+  const response = await sql`SELECT * FROM products`;
+  res.json(response);
 });
 
 module.exports = router;
