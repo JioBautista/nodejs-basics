55a4af20bca1ceb7473cd8b63460160160e83df7
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2025-03-18 13:23:16 -0400

    updates

--- server/app.js
+++ server/app.js
@@ -3,16 +3,10 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const port = 4242;
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
-const invoiceRouter = require("./routes/invoice");
-
 const app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -21,9 +15,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/invoice", invoiceRouter);
-
+app.listen(port, function () {
+  console.log(`Server listening on localhost:${port}`);
+});
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
--- server/routes/invoice.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.json({ user: "jiobautista__" });
-});
-
-module.exports = router;
